2bc645ebf3e97830696030dcf9a3b648b8449726
KrishnaTgBotz <krishnaasingh0012@gmail.com>
2024-11-22 19:29:23 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 
 import logging
 import os
@@ -35,7 +31,7 @@ async def git(bot, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powered by :</b> @VJ_Botz
+<b>Powered by :</b> @KrishnaTg_Botz
 
 """
         if qw.get("description"):
